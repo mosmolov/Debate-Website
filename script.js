456f0f28a7ae8477c9e192fb9779ec6a6fc3dafd
mosmolov <57736073+mosmolov@users.noreply.github.com>
2021-07-13 16:45:05 -0400

    add calendar, about section styling

--- script.js
+++ script.js
@@ -6,4 +6,52 @@ $(document).ready(function(){
             $('.navbar').removeClass("sticky");
         }
     })
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    })
+    document.addEventListener('DOMContentLoaded', function () {
+        var calendarEl = document.getElementById('calendar');
+      
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+      
+            // plugins to load
+            plugins: ['dayGrid', 'timeGrid'], // plugins to load
+      
+            // header controls
+            header: {
+              left: 'dayGridMonth,timeGridWeek,timeGridDay custom1',
+              center: 'title',
+              right: 'custom2 prevYear,prev,next,nextYear' 
+            },
+      
+            // footer controls
+            footer: {
+              left: 'custom1,custom2',
+              center: '',
+              right: 'prev,next' 
+            },
+          
+          // custom toolbar buttons
+          customButtons: {
+            custom1: {
+              text: 'custom 1',
+              click: function () {
+                alert('clicked custom button 1!');
+              } 
+            },
+      
+            custom2: {
+              text: 'custom 2',
+              click: function () {
+                alert('clicked custom button 2!');
+              }
+            } 
+          } 
+      });
+      
+        // render the calendar
+        calendar.render();
+      });
+    
 });
